c17a6828bd5eb0ee2e4d0557fb989a03f96fa99b
Mustafa Şimşek <94194639+mustafa-php@users.noreply.github.com>
2022-09-26 00:25:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("button").click(function () {
+      var secim = $(this).attr("class");
+      var secim = secim.split("-");
+
+      document.execCommand(secim[0], false, secim[1]);
+      $(".icerikblog").focus();
+    });
+  });
